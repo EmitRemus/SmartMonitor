d68dbf9260edead72bc31a0e1e563e6412ba6382
Forelyl <wolf1in1forest@gmail.com>
2025-05-21 14:30:25 +0300

    Update front

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  if (!env.VITE_PORT || Number.isNaN(parseInt(env.VITE_PORT))) {
+    throw new Error(
+      "❌ VITE_PORT is not defined (or is invalid) in your .env file"
+    );
+  }
+
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT), // fallback to default if not set
+    },
+    plugins: [react(), tailwindcss()],
+  };
 });
