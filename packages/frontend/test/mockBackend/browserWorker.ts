d79e4a8a472d3f8b31a9e12d60436ee3644a4e82
Forelyl <wolf1in1forest@gmail.com>
2025-06-01 11:34:11 +0300

    Update browserWorker

--- packages/frontend/test/mockBackend/browserWorker.ts
+++ packages/frontend/test/mockBackend/browserWorker.ts
@@ -1,4 +1,4 @@
-import { allApartmentsHandler } from '@test/mockBackend/handlers/allApartmentsHandler.js';
+import { allApartmentsHandler } from '@test/mockBackend/handlers/allApartmentsHandler';
 import { allBuildingsHandler } from '@test/mockBackend/handlers/allBuildingsHandler';
 import { allPipesHandler } from '@test/mockBackend/handlers/allPipesHandler';
 import { allPumpStationsHandler } from '@test/mockBackend/handlers/allPumpStationsHandler';
